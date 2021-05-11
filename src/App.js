3e8b50f03019b1d079deb7f8ab7787ffd329e4f6
Dawud Rehan <d.rehan7@gmail.com>
2021-05-11 03:44:17 -0400

    init commit, got bkg img going and nav buttons (non functional);

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="header-container">
+        <div className="header-left">
+          <h2 className="top-left-name">Dawud S Rehan</h2>
+        </div>
+        <div className="header-right">
+          <button className="about-button">About</button>
+          <button className="projects-button">Projects</button>
+          <button className="resume-button">Resume</button>
+          <button className="contact-button">Contact</button>
+
+        </div>
+      </div>
+
     </div>
   );
 }
