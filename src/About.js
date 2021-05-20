bd79d9999471087502dcb49d7733211203f0ebbb
Dawud Rehan <d.rehan7@gmail.com>
2021-05-19 21:55:30 -0700

    checkpoint, gonna add more detail and implement react-scroll for clean navigation

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+
+            <h1>ABOUT STUFF GOES HERE</h1>
+
+        </div>
+    )
+}
+
+export default About
+
--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './styles/App.css';
+import About from './About'
 
 function App() {
+
+
+
   return (
     <div className="main-container">
       <div className="header-container">
@@ -8,12 +12,16 @@ function App() {
           <h2 className="top-left-name">Dawud S Rehan</h2>
         </div>
         <div className="header-right">
-          <button className="about-button">About</button>
-          <button className="projects-button">Projects</button>
-          <button className="resume-button">Resume</button>
-          <button className="contact-button">Contact</button>
+          <button className="about-button nav-button">About</button>
+          <button className="projects-button nav-button">Projects</button>
+          <button className="resume-button nav-button">Resume</button>
+          <button className="contact-button nav-button">Contact</button>
 
         </div>
+
+      </div>
+      <div className="secondary-container">
+        <About />
       </div>
 
     </div>
