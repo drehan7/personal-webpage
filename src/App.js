306e6e633beddb222598f5a34eb7b5cb8f0302e8
Dawud Rehan <d.rehan7@gmail.com>
2021-05-25 20:56:56 -0400

    navbar almost done, starting on home component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import './styles/App.css';
-import About from './About'
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Projects from './Components/Projects/Projects'
+import Resume from './Components/Resume/Resume'
+import Contact from './Components/Contact/Contact'
+
 
 function App() {
 
 
 
   return (
-    <div className="main-container">
-      <div className="header-container">
-        <div className="header-left">
-          <h2 className="top-left-name">Dawud S Rehan</h2>
-        </div>
-        <div className="header-right">
-          <button className="about-button nav-button">About</button>
-          <button className="projects-button nav-button">Projects</button>
-          <button className="resume-button nav-button">Resume</button>
-          <button className="contact-button nav-button">Contact</button>
-
-        </div>
-
-      </div>
-      <div className="secondary-container">
-        <About />
-      </div>
-
+    <div className="app">
+      <NavBar />
+      <Home />
+      <About />
+      <Projects />
+      <Resume />
+      <Contact />
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import classes from './Home.module.css'
+
+const Home = () => {
+    return (
+        <div className={classes.Home}>
+            <h1>Home</h1>
+            <p>This is my fuckin about me page motherFUCKERRRRRR GET FUCKED BRAH WICHYO BITCHASS STUPID ASSSSS MUHHHHHHHH FUCKINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN lorem ipsum</p>
+        </div>
+    )
+}
+export default Home
