a376171a00f126616931fe876488b610a9a3044c
Dawud Rehan <d.rehan7@gmail.com>
2021-05-29 20:59:47 -0400

    nav responsive, need to make transitions look better

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,27 +1,39 @@
 import classes from './NavBar.module.css'
+import React, { useState } from 'react'
 const NavBar = () => {
+    const [click, setClick] = useState(false)
+
+
+    const handleClick = () => setClick(!click)
+
+    const mobileMenuClick = () => { setClick(false) }
+
     return (
         <div className={classes.NavBar}>
             <nav>
-                <ul>
-                    <li>
-                        <a href="/#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="/#about">About</a>
-                    </li>
-                    <li>
-                        <a href="/#projects">Projects</a>
-                    </li>
-                    <li>
-                        <a href="/#resume">Resume</a>
-                    </li>
-                    <li>
-                        <a href="/#contact">Contact</a>
-                    </li>
-                </ul>
+                <div>
+                    <ul className={click ? classes.NavMenuActive : classes.NavMenu} >
+                        <li onClick={mobileMenuClick}>
+                            <a href="/#home">Home</a>
+                        </li>
+                        <li onClick={mobileMenuClick}>
+                            <a href="/#about">About</a>
+                        </li>
+                        <li onClick={mobileMenuClick}>
+                            <a href="/#projects">Projects</a>
+                        </li>
+                        <li onClick={mobileMenuClick} >
+                            <a href="/#resume">Resume</a>
+                        </li>
+                        <li onClick={mobileMenuClick}>
+                            <a href="/#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <button onClick={handleClick}><i className={click ? 'fa fa-times' : 'fa fa-bars'}></i></button>
+                </div>
             </nav>
-            <button><i className="fa fa-bars"></i></button>
         </div>
     )
 }
