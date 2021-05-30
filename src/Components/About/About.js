86b7b96d488ac61e0dd1b6ead251cd90250de598
Dawud Rehan <d.rehan7@gmail.com>
2021-05-30 15:02:16 -0400

    about me responsive and has some content

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,8 +7,29 @@ function About() {
                 <h1>About Me</h1>
             </div>
             <div className={classes.AboutMeSection}>
-                <p>I am a Full stack web developer with previous experience in culinary arts and kitchen management</p>
+                <div className={classes.Paragraph}>
+                    <p>I am a Full Stack developer with a <strong>deep love</strong> for learning and building new things which drove my decision to make a change from the <strong>service industry</strong> to tech! Recently completed a 16 week code school where I learned <strong>Full Stack Software Development</strong> skills. While at Momentum, I worked with a remote team of classmates on our capstone project to build a web application that makes cooking and searching recipes easy and fun! I learn very quickly and love to tackle tough problems. I'm excited to continue contributing to the open-source community as well as progress in my <strong>Software Development</strong> career!</p>
+
+                </div>
+
             </div>
+            <div className={classes.Skills}>
+                <ul className={classes.SkillsUL}>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Python</li>
+                    <li>Java</li>
+                    <li>C#</li>
+                    <li>ReactJS</li>
+                    <li>Django & REST</li>
+                    <li>Flask</li>
+                    <li>PostgreSQL</li>
+                    <li>Heroku</li>
+                    <li>Netlify (Hosting)</li>
+                </ul>
+            </div>
+
         </div>
     )
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,22 @@
 import classes from './Home.module.css'
 import Typewriter from 'typewriter-effect'
-
+import { FaArrowDown } from 'react-icons/fa'
 import { SocialIcon } from 'react-social-icons'
 
+
+
 const Home = () => {
+
+
+
     return (
         <div id="home" className={classes.Home}>
             <div className={classes.Middle}>
                 <h1>Hey! I'm Dawud Saiyed Rehan <span>😄 🤙🏽</span> </h1>
                 <div className={classes.Links}>
                     <ul>
-                        <li><SocialIcon network='github' onClick={() => window.open(`https://github.com/drehan7`, '_blank')} fgColor="#fffaff" bgColor="#474749" style={{ height: 75, width: 75, opacity: .9 }} /></li>
-                        <li><SocialIcon network='linkedin' onClick={() => window.open(`https://linkedin.com/in/dsrehan`, '_blank')} fgColor="#fffaff" bgColor="#494947" style={{ height: 75, width: 75, opacity: .8 }} /></li>
+                        <li><SocialIcon network='github' onClick={() => window.open(`https://github.com/drehan7`, '_blank')} fgColor="#fffaff" bgColor="#474749" style={{}} /></li>
+                        <li><SocialIcon network='linkedin' onClick={() => window.open(`https://linkedin.com/in/dsrehan`, '_blank')} fgColor="#fffaff" bgColor="#494947" style={{}} /></li>
                     </ul>
                 </div>
                 <div className={classes.Dynamic}>
@@ -22,6 +27,11 @@ const Home = () => {
                             autoStart: true
                         }} />
                     </h1>
+                </div>
+                <div className={classes.ScrollDown}>
+                    <a href='/#about'>
+                        <button><FaArrowDown></FaArrowDown></button>
+                    </a>
                 </div>
             </div>
         </div>
